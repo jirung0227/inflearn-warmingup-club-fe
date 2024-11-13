78d892cbcc063f58258ae276f40d753090bfa49c
jirung <gw03176370@gmail.com>
2024-11-14 00:20:39 +0900

    feat : 할 일 목록 UI 추가

--- lesson/section3/react-todo-app/src/App.js
+++ lesson/section3/react-todo-app/src/App.js
@@ -1,6 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+
+  todoData = [
+    {
+      id: 1,
+      title: "공부하기",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "운동하기",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "책 읽기",
+      completed: false,
+    },
+  ];
+
   render() {
     return (
       <div className='container'>
@@ -8,6 +43,15 @@ export default class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <p>
+                <input type='checkbox' defaultChecked={false} />
+                {data.title}
+                <button style={this.btnStyle}>x</button>
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     );
