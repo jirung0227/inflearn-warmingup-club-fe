8ce8d6135f3015fc9bc47d2ec3e73a416b636b92
jirung <gw03176370@gmail.com>
2024-11-19 23:06:38 +0900

    feat : TailwindCss로 Todo 앱 스타일링

--- lesson/section4/react-todo-app/src/components/List.js
+++ lesson/section4/react-todo-app/src/components/List.js
@@ -1,23 +1,6 @@
 import React from "react";
 
 export const List = ({ todoData, setTodoData }) => {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  const listStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
   const handleClick = (id) => {
     const newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
@@ -36,18 +19,27 @@ export const List = ({ todoData, setTodoData }) => {
   return (
     <div>
       {todoData.map((data) => (
-        <div style={listStyle(data.completed)} key={data.id}>
-          <p>
-            <input
-              type='checkbox'
-              defaultChecked={false}
-              onChange={() => handleCompleChange(data.id)}
-            />
-            {data.title}
-            <button style={btnStyle} onClick={() => handleClick(data.id)}>
+        <div key={data.id}>
+          <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded'>
+            <div className='items-center'>
+              <input
+                type='checkbox'
+                defaultChecked={false}
+                onChange={() => handleCompleChange(data.id)}
+              />
+              <span className={data.completed ? "line-through" : undefined}>
+                {" "}
+                {data.title}
+              </span>
+            </div>
+
+            <button
+              className='px-4 py-2 float-right'
+              onClick={() => handleClick(data.id)}
+            >
               x
             </button>
-          </p>
+          </div>
         </div>
       ))}
     </div>
