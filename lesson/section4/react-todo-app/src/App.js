cd48e50f31036f9cd794d2d690b7cc336c906cfb
jirung <gw03176370@gmail.com>
2024-11-23 22:54:18 +0900

    feat : drag and drop과 List 컴포넌트 분리

--- lesson/section4/react-todo-app/src/App.js
+++ lesson/section4/react-todo-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
 import { Form } from "./components/Form";
 import { List } from "./components/List";
@@ -18,13 +19,55 @@ export default function App() {
     setValue("");
   };
 
+  const handleDragEnd = (result) => {
+    // 목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
+    if (!result.destination) return;
+
+    const newTodoData = Array.from(todoData);
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+
+    setTodoData(newTodoData);
+  };
+
   return (
     <div className='flex items-center justify-center w-screen h-screen bg-blue-100'>
       <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
         <div className='flex justify-between mb-3'>
           <h1 className='text-3xl font-bold '>할 일 목록</h1>
         </div>
-        <List todoData={todoData} setTodoData={setTodoData} />
+        <div>
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId='todo'>
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {todoData.map((data, index) => (
+                    <Draggable
+                      key={data.id}
+                      draggableId={data.id.toString()}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <List
+                          key={data.id}
+                          id={data.id}
+                          title={data.title}
+                          completed={data.completed}
+                          todoData={todoData}
+                          setTodoData={setTodoData}
+                          provided={provided}
+                          snapshot={snapshot}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+
         <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
     </div>
--- lesson/section4/react-todo-app/src/components/List.js
+++ lesson/section4/react-todo-app/src/components/List.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-export const List = ({ todoData, setTodoData }) => {
+export const List = ({
+  id,
+  title,
+  completed,
+  provided,
+  snapshot,
+  todoData,
+  setTodoData,
+}) => {
   const handleClick = (id) => {
     const newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
@@ -17,68 +24,32 @@ export const List = ({ todoData, setTodoData }) => {
     setTodoData(newTodoData);
   };
 
-  const handleDragEnd = (result) => {
-    // 목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
-    if (!result.destination) return;
-
-    const newTodoData = Array.from(todoData);
-    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-    newTodoData.splice(result.destination.index, 0, reorderedItem);
-
-    setTodoData(newTodoData);
-  };
-
   return (
     <div>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId='todo'>
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {todoData.map((data, index) => (
-                <Draggable
-                  key={data.id}
-                  draggableId={data.id.toString()}
-                  index={index}
-                >
-                  {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      }
+      <div
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"}
                         flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                    >
-                      <div className='items-center'>
-                        <input
-                          type='checkbox'
-                          defaultChecked={false}
-                          onChange={() => handleCompleChange(data.id)}
-                        />
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-                      <button
-                        className='px-4 py-2 float-right'
-                        onClick={() => handleClick(data.id)}
-                      >
-                        x
-                      </button>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      >
+        <div className='items-center'>
+          <input
+            type='checkbox'
+            defaultChecked={false}
+            onChange={() => handleCompleChange(id)}
+          />
+          <span className={completed ? "line-through" : undefined}>
+            {title}
+          </span>
+        </div>
+        <button
+          className='px-4 py-2 float-right'
+          onClick={() => handleClick(id)}
+        >
+          x
+        </button>
+      </div>
     </div>
   );
 };
