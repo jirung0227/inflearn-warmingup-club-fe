cb7a158a2026834acbf290c5a7b50385a1886929
정지형 <jh_jeong@dorosee.kr>
2024-12-09 20:55:45 +0900

    feat : Nav 컴포넌트 추가

--- lesson/section5/netflix-app/src/components/Nav.js
+++ lesson/section5/netflix-app/src/components/Nav.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 export const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+
+      return () => {
+        window.removeEventListener("scroll", () => {});
+      };
+    });
+  }, []);
+
   return (
-    <nav className="nav">
+    <nav className={`nav ${show && "nav__black"}`}>
       <img
         alt="Netflix Logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
